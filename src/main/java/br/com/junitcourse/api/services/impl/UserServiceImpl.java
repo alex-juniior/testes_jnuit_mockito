d79914cf4d58074316177db9210bc8a250f46181
Alex Júnior <alexjunior651@hotmail.com>
2023-06-02 15:52:16 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/junitcourse/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.junitcourse.api.services.impl;
+
+import br.com.junitcourse.api.domain.User;
+
+import br.com.junitcourse.api.repositories.UserRepository;
+import br.com.junitcourse.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
