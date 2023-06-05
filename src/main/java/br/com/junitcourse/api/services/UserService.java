01d20286738a980342ceeb2df64175b7a959d49c
Alex Júnior <alexjunior651@hotmail.com>
2023-06-05 08:40:59 -0300

    Endpoint findAll para user

--- src/main/java/br/com/junitcourse/api/services/UserService.java
+++ src/main/java/br/com/junitcourse/api/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.junitcourse.api.services;
 
 import br.com.junitcourse.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 
 }
--- src/main/java/br/com/junitcourse/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/junitcourse/api/services/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import br.com.junitcourse.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,9 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow( ()-> new ObjectNotFoundException("Objeto não encontrada"));
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
