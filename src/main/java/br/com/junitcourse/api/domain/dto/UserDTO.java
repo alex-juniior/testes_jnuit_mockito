6065a87d462e65ad991530b50f42ed5048870023
Alex Júnior <alexjunior651@hotmail.com>
2023-06-05 08:28:36 -0300

    Retornando dto para user resource

--- src/main/java/br/com/junitcourse/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/junitcourse/api/domain/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package br.com.junitcourse.api.domain.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class UserDTO {
     private Integer id;
     private String name;
     private String email;
+    @JsonIgnore
     private String password;
 
 }
